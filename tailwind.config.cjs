b9bf2d6ee2d2d8c0513c797af5ebd37a45e14cfb
Nazar <nazar.poplavsky@gmail.com>
2023-03-31 15:44:23 +0200

    First commit publish

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			colors: {
+				blue: "#4E7BBE",
+				green: "#3CAD55",
+				yellow: "#FBBC15",
+				red: "#E94337",
+			},
+			fontSize: {
+				sm: "0.875rem",
+				base: "1rem",
+				lg: "1.25rem",
+				xl: "1.5rem",
+				"2xl": "2.5rem",
+			},
+			backgroundImage: {
+				hero: "url('/assets/images/hero.jpg')",
+			},
+		},
+	},
+	plugins: [],
+};
