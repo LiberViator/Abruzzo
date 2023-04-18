03030f2d1b377c8c7b8b35e29c498d81be95a013
Nazar <nazar.poplavsky@gmail.com>
2023-04-18 13:36:27 +0200

    Fixed card grid responsiveness

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -2,9 +2,9 @@ import EmblaCarousel from "embla-carousel";
 
 const emblaNode = document.querySelector(".embla");
 const options = {
-	align: "start",
+	align: "center",
 	breakpoints: {
-		"(min-width: 1024px)": { active: false },
+		"(min-width: 768px)": { active: false },
 	},
 };
 
